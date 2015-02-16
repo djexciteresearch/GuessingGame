abbd97777962e8902fb24d9ecd47b9b15133bbb8
DJ Nadgar <dj@exciteresearch.com>
2015-02-16 01:28:02 -0500

    Some changes to javascript file

--- guessingGame.js
+++ guessingGame.js
@@ -5,7 +5,7 @@
  * [/] When a game begins, there should be a random number generated between 1-100.
  * [/] The user should have an input field where they can submit a guess.
  * [/] After the user submits a guess, indicate whether their guess is 'hot' or 'cold'. Let the user know if they need to guess higher or lower.
- * [ ] Allow the user to guess only a certain amount of times. When they run out of guesses let them know the game is over.
+ * [/] Allow the user to guess only a certain amount of times. When they run out of guesses let them know the game is over.
  * [/] Feel free to use prompts to get user input on your first version.
  * [/] For the final version of your project, you'll need to create an HTML-based interface for getting user inputs and giving feedback on guesses.
  * [/] Validate inputs that they are real numbers between 1-100.
@@ -13,7 +13,8 @@
  * [ ] Store all of the guesses and create a way to check if the guess is a repeat.
  * [/] Track the user's previous guess. Let them know if they are getting “hotter” or “colder” based on their previous guess.
  * [/] Create a button that provides the answer (Give me a Hint).
- * [ ] Submit the guess by pressing enter or clicking the submit button.
+ * [ ] Submit the guess by pressing enter 
+ * [ ] or clicking the submit button.
  * [ ] After a user guesses a number keep a visual list of Hot and Cold answers that the user can see.
  * [ ] Change the background color, add an image, or do something creative when the user guesses the correct answer.
  */
@@ -128,7 +129,7 @@ launcher.prototype.display= function(text){
 	$("#disposition").html(text);
 }
 
-launcher.prototype.evalGuess= function(num){
+launcher.prototype.evalGuess = function(num){
 	this.decrease();
 	this.updateClock();
 	this.guesses.push(num);
@@ -157,7 +158,7 @@ launcher.prototype.evalGuess= function(num){
 		} else {
 			evalStr+=" guess higher.";
 		}
-		evalStr += "Try again.";
+		evalStr += " Try again.";
 	}
 	this.display(evalStr);
 	this.gameOverTest();
