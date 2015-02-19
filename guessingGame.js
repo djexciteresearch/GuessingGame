e9e7819de564233da7c75c7800486ac3e19001f5
DJ Nadgar <dj@exciteresearch.com>
2015-02-19 16:08:53 -0500

    almost complete Guessing Game. visual list is not working and changing the background or doing something cool is not implemeted yet

--- guessingGame.js
+++ guessingGame.js
@@ -10,13 +10,16 @@
  * [/] For the final version of your project, you'll need to create an HTML-based interface for getting user inputs and giving feedback on guesses.
  * [/] Validate inputs that they are real numbers between 1-100.
  * [/] Create a new game button that resets the game.
- * [ ] Store all of the guesses and create a way to check if the guess is a repeat.
+ * [/] Store all of the guesses and create a way to check if the guess is a repeat.
  * [/] Track the user's previous guess. Let them know if they are getting “hotter” or “colder” based on their previous guess.
  * [/] Create a button that provides the answer (Give me a Hint).
- * [ ] Submit the guess by pressing enter 
- * [ ] or clicking the submit button.
- * [ ] After a user guesses a number keep a visual list of Hot and Cold answers that the user can see.
- * [ ] Change the background color, add an image, or do something creative when the user guesses the correct answer.
+ * [/] Submit the guess by pressing enter 
+ * [/] or clicking the submit button.
+ * [!] After a user guesses a number keep a visual list of Hot and Cold answers that the user can see.
+ * [ ] (extra credit) Change the background color, add an image, or do something creative when the user guesses the correct answer.
+ * [/] Prevent play after 1) countdown=0 2) guessSuccess
+ * [ ] (extra credit) Use images for the enter and panic button
+ * [ ] (extra credit) document onload play audio "Shall we play a game"
  */
 
 // Generate Random Number 1-100
@@ -44,7 +47,7 @@ function primeControls(countdown){
 		//evaluate on enter key
 		if ( e.which == 13 ) {
 			e.preventDefault();
-			countdown.evalGuess($( "#guess" ).val());
+			countdown.authenticate($( "#guess" ).val());
 			$( "#guess" ).val("Input Number 1-100");
 		}
 	});
@@ -59,7 +62,7 @@ function primeControls(countdown){
 			$( "#guess" ).val("Input Number 1-100");
 		} else if(!isNaN($( "#guess" ).val())){
 		//evaluate guess
-			countdown.evalGuess($( "#guess" ).val());
+			countdown.authenticate($( "#guess" ).val());
 			$( "#guess" ).val("Input Number 1-100");
 		}
 	}).keydown(function( e ) {
@@ -71,13 +74,24 @@ function primeControls(countdown){
 	//enter button field
 	$( "#submit" ).click(function(){
 		//do nothing because input.blur works already
-		//on click text to "Boom!
-		//on release text to "Enter"
+		//if empty add default text into input field
+		if ($( "#guess" ).val()==""){
+			$( "#guess" ).val("Input Number 1-100");
+		} else if (isNaN($( "#guess" ).val())){
+		//cynical version - input is not a number reduce guess by and prompt
+		//input is not a number just try another input
+			$( "#disposition" ).val("> That's not a number. Try again.")
+			$( "#guess" ).val("Input Number 1-100");
+		} else if(!isNaN($( "#guess" ).val())){
+		//evaluate guess
+			countdown.authenticate($( "#guess" ).val());
+			$( "#guess" ).val("Input Number 1-100");
+		}
 	});
 	//Play Again button field
 	$( "#playAgain" ).click(function(){
 		//on click execute reset sequence
-		countdown.reset();
+		countdown.init();
 	});
 	//enter button field
 	$( "#hint" ).click(function(){
@@ -93,11 +107,15 @@ var launcher = function(){
 	this.myNum = 0;
 	this.guesses = [];
 	this.controls = {};
+	this.GameOver;
+	this.hotColdList = []; //["1(cold)","100(cold)",...]
 }
 
-launcher.prototype.reset = function(){
+launcher.prototype.init = function(){
+	this.GameOver = false;
 	this.count = maxAttempts;
 	this.guesses = [];
+	this.hotColdList = [];
 	this.myNum = this.acquire();
 	this.updateClock(this.count);
 	this.display("> Guess the number I am thinking of.");
@@ -116,13 +134,32 @@ launcher.prototype.updateClock = function(){
 	$("#guessed").html(this.count);
 }
 
-launcher.prototype.gameOverTest = function(){
+launcher.prototype.authenticate = function(num){
 	if(this.count<=0){
+		//Game Over Test - do nothing if game over else
+		this.GameOver = true;
+		console.log("ignition");
 		this.display("> Missile(s) launched! GAME OVER man... Play Again?");
+		//no need to 
 		//unset input.click.keydown submit.click hint.click
+	} else if (this.myNum == num) {
+		this.GameOver = true;
+		console.log(this.myNum, num);
+		//Guess Success Test - update display with success message else
+		this.display("> Code authenticated. Launch aborted. Play again?");
+	} else if ( this.GameOver != true ){
+		//update guest register
+		//decrease count
+		//update clock
+		//weigh response - evaluate guess
+		//update terminal display
+		//this.myNum == this.guesses[this.guesses.length-1]		
+		this.display(this.evalGuess(num));
 	} else {
-		console.log(this.count);
+		this.display("Press the Panic Button!");
 	}
+
+	
 }
 
 launcher.prototype.display= function(text){
@@ -130,18 +167,30 @@ launcher.prototype.display= function(text){
 }
 
 launcher.prototype.evalGuess = function(num){
+	var evalStr;
+
+	this.guesses.push(num);
+	//continue countdown, evaluate guess
 	this.decrease();
 	this.updateClock();
-	this.guesses.push(num);
-	var evalStr="> Your guess is";
-	
-	if (this.myNum == this.guesses[this.guesses.length-1]) {
-		evalStr="> Code authenticated. Launch aborted. Play again?";
-	} else {
+	//was this number guessed before?
+	var sameAsPrev = false;
+	for(var i=0;i<this.guesses.length-1;i++){ //length-1 no curr guess
+		if (this.guesses[i] == num ){
+			console.log(this.guesses[i] , num);
+			evalStr="> You guessed "+num+" before. Try Again.";
+			sameAsPrev = true;
+		}
+	}
+	//formulate response for incorrect guess
+	if(!sameAsPrev){
+		evalStr="> Your guess is";
 		if (Math.abs(this.myNum - this.guesses[this.guesses.length-1]) < 10 ) {
 			evalStr+=" hot,";
+			this.hotColdList.push(num+"(hot)");
 		} else {
 			evalStr+=" cold,";
+			this.hotColdList.push(num+"(cold)");
 		}
 		
 		if (2<=this.guesses.length){40
@@ -160,21 +209,28 @@ launcher.prototype.evalGuess = function(num){
 		}
 		evalStr += " Try again.";
 	}
-	this.display(evalStr);
-	this.gameOverTest();
+	this.displayHotColdList();
+	return evalStr;
 }
 
 launcher.prototype.hint= function(){
 	this.display("> Backdoor hacked; I was thinking of "+this.myNum+".");
 }
 
-var controller = new launcher();
+launcher.prototype.displayHotColdList= function(){
+	var str="Previous Guesses: ";
+	for(i=0;i<this.hotColdList.length;i++){
+		str+=this.hotColdList[i]+", ";
+	}
+	//str.trim();
+	$("#guessList").innerHTML="TEST";//str.slice(0,-2);
+	console.log(str.slice(0,-2));
+}
 
-controller.reset();
-primeControls(controller);
+var missile = new launcher();
 
-//addListeners to Input, Enter, Play Again, Panic
-//where onClick evaluates element for appropriate action
+missile.init();
+primeControls(missile);
 
 
 
